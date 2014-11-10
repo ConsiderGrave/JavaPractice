fbb2e6a43048e1220005fec820a37c5ebc7c0db3
Elisa C Bils <graveheart777@gmail.com>
2014-11-10 13:47:11 -0500

    First commit of car speed project.

---
+++ src/Sparks.java
@@ -0,0 +1,50 @@
+
+public class Sparks {
+	
+  public static void main(String[]args){
+		
+	int speed = 1;
+		
+		
+	 do {
+		
+		
+		 System.out.println("You are driving " + speed + " mph.");
+		
+		
+		speed++;
+		
+		
+		 if (speed <= 40){
+			
+			System.out.println("You are going under the minimum speed limit.");
+		
+		 }
+	
+		
+		 if (speed < 60){
+			
+			System.out.println("You are going under the maximum speed limit.");
+			
+		 }
+		
+		 if (speed > 60){
+			
+			System.out.println("You are going over the speed limit.");
+		
+		 }
+		
+		 if (speed == 60){
+			
+			System.out.println("You are staying within the speed limit");
+		
+		 }
+		 
+		
+	
+	 } while(speed <= 100);
+   
+
+  }
+
+}
